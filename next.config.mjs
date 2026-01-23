93610a826a36c8f37842cc952bd4c93175eb7b52
Vibhor <vibhordeopa@gmail.com>
2026-01-23 12:31:55 +0530

    Remove static export to allow dynamic routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
-    unoptimized: true
-  }
+    domains: ["lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
